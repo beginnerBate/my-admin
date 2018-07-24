dc60d5d120d98bb9e17b7c5131dbc747411bfc68
liuyeqian <liuyeq2015@163.com>
2018-07-24 09:53:46 +0800

    gg

---
+++ src/components/reg-book/rb-stepthree.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="complete-info">
+    <div class="user-info">
+      <span>姓名: XXX</span>
+      <span>性别: 男</span>
+      <span>身份证号: xxxxxxxxxxxxxxxxxx</span>
+    </div>
+    <div>
+      <div>手机号: 18360451082</div>
+      <div>键盘</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
